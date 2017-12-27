c1c44c17efa82335257c543cb8b52ef05ac83cde
rinkom <32238693+rinkom@users.noreply.github.com>
2017-12-27 13:12:56 +0900

    Update port_size.py

--- Codes/port_size.py
+++ Codes/port_size.py
@@ -12,7 +12,7 @@ def organize_s2compare():
     # 2. delete unnecessary rows if major_region is not in majorreg_list
     # 3. define cerebral nuclei to be Striatum and Pallidum combined
 
-    df = pd.read_csv('~/Desktop/WBCA-master/raw_data/s2_compare.csv', index_col=0, header=0)
+    df = pd.read_csv('~/Desktop/WBCA-master/DataAnalysis/s2_compare.csv', index_col=0, header=0)
 
     delete_list = ['ID', 'Voxel_so_2016', 'Voxel_oh_bef2014']
     df = df.drop(delete_list, 1)
@@ -27,13 +27,13 @@ def organize_s2compare():
         if row[2] == 'Striatum' or row[2] == 'Pallidum':
             df.at[key, 'Major_Region'] = 'Cerebral nuclei'
 
-    df.to_csv('~/Desktop/WBCA-master/WBCA2017/voxel_region.csv', header=True, index=True)
+    df.to_csv('~/Desktop/WBCA-master/Release/voxel_region.csv', header=True, index=True)
 
 def reglist_unique():
     # the region acronym in row and column of conn_matrix do not match.
     # from conn matrix, create two columns (From and To) with unique region names → (292 rows * 2 columns)
 
-    conn_matrix = pd.read_csv('~/Desktop/WBCA-master/WBCA2017/conn_matrix.csv', index_col=0, header=0)
+    conn_matrix = pd.read_csv('~/Desktop/WBCA-master/Release/conn_matrix.csv', index_col=0, header=0)
 
     new_acronym_list = np.unique(list(conn_matrix.index) + list(conn_matrix.columns))
     print(len(new_acronym_list))
@@ -44,14 +44,14 @@ def reglist_unique():
 
     df1 = pd.DataFrame(np.column_stack([col1, col2]), columns=['From', 'To'])
     
-    df1.to_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv')
+    df1.to_csv('~/Desktop/WBCA-master/Release/finale.csv')
 
 def voxel_vs_connstr():
     # 1. print out volume(voxel) of region in [From],[To], and also the value that multiplies the two
     # 2. prints out a voxel**2 vs. connectivity graph
 
-    df1 = pd.read_csv('~/Desktop/WBCA-master/WBCA2017/voxel_region.csv', index_col=0, header=0)
-    df2 = pd.read_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv', index_col=0, header=0)
+    df1 = pd.read_csv('~/Desktop/WBCA-master/Release/voxel_region.csv', index_col=0, header=0)
+    df2 = pd.read_csv('~/Desktop/WBCA-master/Release/finale.csv', index_col=0, header=0)
 
     temp1 = []
     for acronym in df2['From']:
@@ -69,14 +69,14 @@ def voxel_vs_connstr():
 
     df2['V(s)*V(t)'] = df2['V(s)'] * df2['V(t)']
 
-    df2.to_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv', header=True, index=True)
+    df2.to_csv('~/Desktop/WBCA-master/Release/finale.csv', header=True, index=True)
 
 
 def num_cell_method2():
     # convert voxel -> num_cell of each region
 
-    df1 = pd.read_csv('~/Desktop/WBCA-master/WBCA2017/voxel_region.csv', index_col=0, header=0)
-    df2 = pd.read_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv', index_col=0, header=0)
+    df1 = pd.read_csv('~/Desktop/WBCA-master/Release/voxel_region.csv', index_col=0, header=0)
+    df2 = pd.read_csv('~/Desktop/WBCA-master/Release/finale.csv', index_col=0, header=0)
 
     num_cell_dict = {'Cerebellum': 40000000, 'Isocortex': 8000000, 'Hippocampal formation': 5000000,
                      'Olfactory areas': 1000000, 'Cortical subplate': 1000000,
@@ -95,13 +95,13 @@ def num_cell_method2():
         v = tmp['V(s)'] * num_cell_dict[major_region] / total_voxel_dict[major_region]
         temp.append(v)
     df2['num_cell(from)'] = temp
-    df2.to_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv')
+    df2.to_csv('~/Desktop/WBCA-master/Release/finale.csv')
 
 def connectivitystr():
     # fill in connectivity_strength_row with the matching (From)->(To) pair
 
-    df = pd.read_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv', index_col=0, header=0)
-    df2 = pd.read_csv('~/Desktop/WBCA-master/WBCA2017/conn_matrix.csv', index_col=0, header=0)
+    df = pd.read_csv('~/Desktop/WBCA-master/Release/finale.csv', index_col=0, header=0)
+    df2 = pd.read_csv('~/Desktop/WBCA-master/Release/conn_matrix.csv', index_col=0, header=0)
     temp = []
     for x, y in zip(df['From'],df['To']):
         try:
@@ -109,13 +109,13 @@ def connectivitystr():
         except:
             temp.append(0.0)
     df['Connectivity_Strength'] = temp
-    df.to_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv')
+    df.to_csv('~/Desktop/WBCA-master/Release/finale.csv')
 
 
 def totalcs():
     #total conn_str from [From reg]
 
-    df = pd.read_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv',index_col=0,header=0)
+    df = pd.read_csv('~/Desktop/WBCA-master/Release/finale.csv',index_col=0,header=0)
 
     acronym_list = np.unique([x for x in df['From']])
     total_cs = {}
@@ -126,26 +126,26 @@ def totalcs():
     totalcsList = [total_cs['{}'.format(acr)] for acr in df['From']]
     df['total_cs_from'] = totalcsList
 
-    df.to_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv', index=True)
+    df.to_csv('~/Desktop/WBCA-master/Release/finale.csv', index=True)
 
 def n_out():
     # output port-size
 
-    df = pd.read_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv', index_col=0, header=0)
+    df = pd.read_csv('~/Desktop/WBCA-master/Release/finale.csv', index_col=0, header=0)
     df['n_out'] = df['num_cell(from)'] * df['Connectivity_Strength'] / df['total_cs_from']
     df= df.fillna(0.0)
-    df.to_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv')
+    df.to_csv('~/Desktop/WBCA-master/Release/finale.csv')
 
 def matrix():
     # create a matrix with value of port-size
 
-    df = pd.read_csv('~/Desktop/WBCA-master/WBCA2017/finale.csv',index_col=0,header=0)
+    df = pd.read_csv('~/Desktop/WBCA-master/Release/finale.csv',index_col=0,header=0)
     temp = np.unique(df['From'])
     df2 = pd.DataFrame(columns=temp, index= temp)
     for x, y, z in zip(df['From'], df['To'], df['n_out']):
        # print(x, y , z)
         df2.at[x,y] = z
-    df2.to_csv('~/Desktop/WBCA-master/WBCA2017/finale_matrix.csv', index=True)
+    df2.to_csv('~/Desktop/WBCA-master/Release/finale_matrix.csv', index=True)
 
 if __name__ == '__main__':
     organize_s2compare()
